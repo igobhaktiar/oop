bf2aa1166d05b28d913440db4ee00fd89627f51b
Firgo Bhaktiar Hamsah <71805833+igobhaktiar@users.noreply.github.com>
2023-09-11 00:24:52 +0700

    mixxin

---
+++ bin/extensionMethod.dart
@@ -0,0 +1,11 @@
+import 'extension.dart';
+
+void main(){
+  var unsortNumbers = [2, 5, 3, 1, 4];
+  print(unsortNumbers);
+
+  var sortedNumbers = unsortNumbers.sortAsc();
+
+  print(sortedNumbers);
+}
+
